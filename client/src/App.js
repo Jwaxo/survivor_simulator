1cf297cbd46e59b6252d83d67a78ca77a9965415
Jessica Wayde <jess@electriccitizen.com>
2025-02-19 10:12:14 -0800

    get interface state inheritence working and theme actions

--- client/src/App.js
+++ client/src/App.js
@@ -14,21 +14,6 @@ seedrandom(random_seed, { global: true});
 
 season.generateTribes();
 
-if (season.state.log.length < 1) {
-  season.addToLog("A new season begins!");
-}
-if (season.state.log.length === 1) {
-  season.addToLog("Welcome new players.");
-}
-
-// Advance Time isn't bubbling up to the StatusBox as expected.
-// Updating time changes the State of Season--but since Season isn't rendering
-// StatusBox, that doesn't matter. Interface handling Time would update
-// everything accordingly. So maybe Interface handles moving the information
-// about based on player interaction, and state tracks that info--but doesn't
-// bubble it back up to Interface.
-
-
 function App() {
 
   return (
--- client/src/src/Interface/Interface.js
+++ client/src/src/Interface/Interface.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import ActionBox from './Actionbox';
 import EnviroBox from './Envirobox';
 import InfoBox from './Infobox';
@@ -18,15 +18,14 @@ import Action from '../Action/Action';
  * 5. StatusBox naturally updates with the new time.
  */
 
-function Interface({season, timePerTic, debug}) {
+export default function Interface({season, timePerTic, debug}) {
   const [time, setTime] = useState('12:00 AM');
   const [actions, setActions] = useState([]);
-
-  if (actions?.length < 1) {
-    addAction('Advance Time', () => {
-      advanceTime();
-    });
-  }
+  const [log, setLog] = useState([]);
+  const [players, setPlayers] = useState(() => season.getPlayers());
+  const stateRef = useRef();
+  stateRef.log = log;
+  stateRef.actions = actions;
 
   function advanceTime() {
     season.advanceTime();
@@ -36,9 +35,48 @@ function Interface({season, timePerTic, debug}) {
   function addAction(label = 'Action', callback = () => {
     console.log("No callback set for this action.")
   }, type = 'button') {
-    setActions([...actions, <Action label={ label } type={ type } callback={ callback }/>
+    setActions([...stateRef.actions, <Action label={ label } type={ type } callback={ callback }/>
     ]);
   }
+
+  function addActions(newActions = []) {
+    setActions([...stateRef.actions,
+      ...newActions.map(action => (
+        <Action label={ action.label } type={ action.type } callback={ action.callback }/>
+      ))
+    ]);
+  }
+
+  function addToLog(message = "") {
+    if (message !== "") {
+      season.addToLog(message);
+      setLog([...stateRef.log, message]);
+    }
+  }
+
+  // Functions to run on first render.
+  useEffect(() => {
+    if (stateRef.actions?.length < 1) {
+      addActions([
+        {
+          label: 'Advance Time',
+          callback: () => {
+            advanceTime();
+          },
+          type: 'button'
+        },
+        {
+          label: 'Add Action',
+          type: 'button',
+          callback: () => {
+            addAction('Add Message', () => {
+              addToLog("Adding a new log entry");
+            })
+          }
+        }
+      ]);
+    }
+  }, []);
 
   return (
     <div className="interface">
@@ -48,17 +86,15 @@ function Interface({season, timePerTic, debug}) {
         </div>
         <div className="interface-panel interface-main">
           <EnviroBox />
-          <InfoBox log={ season.state.log }/>
+          <InfoBox log={ log }/>
         </div>
         <div className="interface-panel interface-side">
-          <PlayerBox players={ season.getPlayers() } />
+          <PlayerBox players={ players } />
         </div>
         <div className="interface-panel interface-bottom">
-          <ActionBox actions={ actions } addAction={ addAction } />
+          <ActionBox actions={ actions } addAction={ addActions } />
         </div>
       </div>
     </div>
   )
 }
-
-export default Interface;
